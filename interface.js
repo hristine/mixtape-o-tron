de26ba78e43fa9da5c0be5831eebe74438847b81
Christine Davis <christine@neato.co.nz>
2013-08-10 13:30:07 -0400

    Adds a bunch of stuff that allows you to get a list of artists which are similar; and prevent you from doing searches that you cannot do.

--- interface.js
+++ interface.js
@@ -3,12 +3,17 @@ var nestapikey = "LHDCAIKGCIJUU8PO3",
 	nest = nest.nest(nestapikey),
 	songsTemplate = Handlebars.compile([
 		'{{#each results }}',
-		'<li data-song-id="{{ id }}" class="song"><b>{{ title }}</b> - {{ artist_name }}</li>',
+		'<li data-song-id="{{ id }}" data-artist-id="{{ artist_id }}" class="song"><span class="includeSong">&#8679;</span><span class="removeSong">&#215;</span><b>{{ title }}</b> - {{ artist_name }} <button class="moreArtists">More Artist Suggestions</button></li>',
 		'{{/each }}'
 	].join('')),
 	termsTemplate = Handlebars.compile([
 		'{{# each terms }}',
 		'<option value="{{ name }}">{{ name }}</option>',
+		'{{/each }}'
+	].join('')),
+	artistsTemplate = Handlebars.compile([
+		'{{# each artists }}',
+		'<div data-artist-id="{{ id }}" class="artistName">{{ name }}</div>',
 		'{{/each }}'
 	].join(''));
 
@@ -25,10 +30,41 @@ function findMoreSongs() {
 	nest.searchSongs(args, listMoreSongs);
 }
 
+function listSimilarArtists(error, results) {
+	jQuery('#artists').append(artistsTemplate({artists: results.artists}));
+}
+function findSimilarArtists() {
+	var artistId = jQuery(this).parents('li').attr('data-artist-id'),
+		artist = nest.artist({id: artistId});
+	artist.similar(listSimilarArtists);
+}
+function addArtistToCriteria() {
+	jQuery('.criteria[name="artist"]').val(jQuery(this).text());
+	// These are mutually exclusive.
+	jQuery('.criteria[name="mood"]').val('');
+	jQuery('.criteria[name="style"]').val('');
+	findMoreSongs();
+}
+
 function moveSongToPlaylist() {
-	var elt = jQuery(this);
+	var elt = jQuery(this).parents('li');
 	jQuery('#keepit').append(elt);
 	jQuery('#keepit .none').hide();
+}
+function removeSongFromPlaylist() {
+	jQuery(this).parents('li').remove();
+	if (jQuery('#keepit').children().length === 1) {
+		jQuery('#keepit .none').show();
+	}
+}
+
+function checkCriteria() {
+	if (jQuery(this).is('select')) {
+		jQuery('.criteria[name="artist"]').val('');
+		jQuery('.criteria[name="title"]').val('');
+	} else {
+		jQuery('select[name="mood"], select[name="style"]').val('');
+	}
 }
 
 function loadTerms(termType) {
@@ -48,5 +84,15 @@ jQuery(document).ready(function () {
 	loadTerms('mood');
 	loadTerms('style');
 	jQuery('#doSearch').click(findMoreSongs);
-	jQuery("#foundit").on('click', '.song', moveSongToPlaylist);
+	jQuery('select, input')
+		.on('change', checkCriteria);
+	jQuery("#foundit")
+		.on('click', '.includeSong', moveSongToPlaylist)
+		.on('click', '.moreArtists', findSimilarArtists);
+	jQuery('#keepit')
+		.on('click', '.moreArtists', findSimilarArtists)
+		.on('click', '.removeSong', removeSongFromPlaylist);
+	jQuery('#artists')
+		.on('click', '.artistName', addArtistToCriteria);
+
 });

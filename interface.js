83f55373bdddc5900dbcdf2c554a03e31f77039c
Christine Davis <christine@neato.co.nz>
2013-08-10 12:27:41 -0400

    Allow choosing a mood or style when searching for songs.

--- interface.js
+++ interface.js
@@ -1,7 +1,14 @@
-var nest = nest.nest("LHDCAIKGCIJUU8PO3"),
+
+var nestapikey = "LHDCAIKGCIJUU8PO3",
+	nest = nest.nest(nestapikey),
 	songsTemplate = Handlebars.compile([
 		'{{#each results }}',
 		'<li data-song-id="{{ id }}" class="song"><b>{{ title }}</b> - {{ artist_name }}</li>',
+		'{{/each }}'
+	].join('')),
+	termsTemplate = Handlebars.compile([
+		'{{# each terms }}',
+		'<option value="{{ name }}">{{ name }}</option>',
 		'{{/each }}'
 	].join(''));
 
@@ -24,7 +31,22 @@ function moveSongToPlaylist() {
 	jQuery('#keepit .none').hide();
 }
 
+function loadTerms(termType) {
+	var args = {
+		api_key:nestapikey,
+		type: termType
+	};
+	function listTerms (data) {
+		var elt = jQuery('[name="' + termType + '"]');
+		elt.append(termsTemplate({terms: data.response.terms}));
+	}
+
+	jQuery.get('http://developer.echonest.com/api/v4/artist/list_terms', args, listTerms);
+}
+
 jQuery(document).ready(function () {
+	loadTerms('mood');
+	loadTerms('style');
 	jQuery('#doSearch').click(findMoreSongs);
 	jQuery("#foundit").on('click', '.song', moveSongToPlaylist);
 });
